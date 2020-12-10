980cee37125ab0d0ece9b3e24e717e9061c4851e
Logan Kearsley <chronosurfer@gmail.com>
2020-12-10 16:56:40 -0700

    Experimenting with digit-alignment shortcuts

--- src/index.ts
+++ src/index.ts
@@ -31,10 +31,17 @@ function * passthru<A, B>(digits: Iterable<A>, sym2val: Map<A, bigint>, to: B[])
   }
 }
 
+function digit_ratio(a: number, b: number) {
+  let x = a;
+  while (b) [b, a] = [a % b, b];
+  return x / a;
+}
+
 export class BaseConverter<A,B> {
   private from: Map<A, bigint>;
   private fb: bigint;
   private tb: bigint;
+  private in_digits: number;
   private passthru: boolean;
 
   constructor(from: TwoPlus<A>, private to: TwoPlus<B>) {
@@ -42,13 +49,29 @@ export class BaseConverter<A,B> {
     this.fb = BigInt(from.length);
     this.tb = BigInt(to.length);
     this.passthru = this.fb === this.tb;
+    this.in_digits = digit_ratio(from.length, to.length);
   }
 
   * convertLE(digits: Iterable<A>) {
     const { from: sym2val, to, tb } = this;
     if (this.passthru) yield * passthru(digits, sym2val, to);
     else {
-      let n = this.fromLE(digits);
+      const { in_digits: p, fb: radix } = this;
+      let [n, m, c] = [0n, 1n, 1];
+      for (const d of digits) {
+        const t = sym2val.get(d);
+        if (t === void 0) throw new Error("Unrecognized digit");
+        n += t * m;
+        if (c === p) {
+          for (; n > 0n; n/= tb) yield to[Number(n % tb)];
+          m = 1n;
+          c = 1;
+        } else {
+          c++;
+          m *= radix;
+        }
+      }
+
       for (; n > 0n; n/= tb) yield to[Number(n % tb)];
     }
   }

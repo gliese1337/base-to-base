f8a793bf83b9f218ba547851d6d172648bc53dbf
Logan Kearsley <chronosurfer@gmail.com>
2020-12-11 16:28:49 -0700

    Add bijective tests.

--- test/exhaustive-test.ts
+++ test/exhaustive-test.ts
@@ -30,6 +30,24 @@ for(let radix = 2; radix <= 120; radix += 3) {
   });
 }
 
+for(let radix = 3; radix <= 120; radix += 3) {
+  describe(`Converting to and from bijective base ${ radix-1 }`, () => {
+    const symbols = Array.from({ length: radix }, (_, i) => i) as TwoPlus<number>;
+    const c = new BaseConverter({ symbols, bijective: true }, { symbols, bijective: true });
+    for (const n of integers) {
+      it(`should roundtrip ${ n } through bijective base ${ radix-1 } in big-endian representation`, () => {
+        const m = c.fromBE(c.toBE(n));
+        expect(m).to.eql(n);
+      });
+      
+      it(`should roundtrip ${ n } through bijective base ${ radix-1 } in little-endian representation`, () => {
+        const m = c.fromLE(c.toLE(n));
+        expect(m).to.eql(n);
+      });
+    }
+  });
+}
+
 describe("Interbase Conversion tests", () => {
   for(let r1 = 2; r1 <= 12; ++r1) {
     const from = Array.from({ length: r1 }, (_, i) => i+1) as TwoPlus<number>;  
@@ -62,6 +80,63 @@ describe("Interbase Conversion tests", () => {
           });
         }
       });
+
+      if (r1 > 2 && r2 > 2) {
+        const cb1 = new BaseConverter(from, { symbols: to, bijective: true });
+        describe(`Converting from base ${ r1 } to bijective base ${ r2 - 1 }`, () => {
+          for (const n of integers) {
+            it(`should roundtrip ${ n } through bases ${ r1 } and bijective ${ r2 - 1 } in big-endian representation`, () => {
+              const source = BaseConverter.toBE(n, from, false);
+              const target = BaseConverter.toBE(n, to, true);
+              
+              const test = cb1.convertBE(source);
+              expect(test).to.eql(target);
+
+              const m = BaseConverter.fromBE(test, to, true);
+              expect(''+m).to.eql(''+n);
+            });
+
+            it(`should roundtrip ${ n } through bases ${ r1 } and bijective ${ r2 - 1 } in little-endian representation`, () => {
+              const source = BaseConverter.toLE(n, from, false);
+              const target = [...BaseConverter.toLE(n, to, true)];
+              
+              const test = [...cb1.convertLE(source)];
+              expect(test).to.eql(target);
+
+              const m = BaseConverter.fromLE(test, to, true);
+              expect(''+m).to.eql(''+n);
+            });
+          }
+        });
+
+        
+        const cb2 = new BaseConverter({ symbols: from, bijective: true }, to);
+        describe(`Converting from bijective base ${ r1 - 1 } to base ${ r2 }`, () => {
+          for (const n of integers) {
+            it(`should roundtrip ${ n } through bases bijective ${ r1 - 1 } and ${ r2 } in big-endian representation`, () => {
+              const source = BaseConverter.toBE(n, from, true);
+              const target = BaseConverter.toBE(n, to, false);
+              
+              const test = cb2.convertBE(source);
+              expect(test).to.eql(target);
+
+              const m = BaseConverter.fromBE(test, to, false);
+              expect(''+m).to.eql(''+n);
+            });
+
+            it(`should roundtrip ${ n } through bases bijective ${ r1 - 1 } and ${ r2 } in little-endian representation`, () => {
+              const source = BaseConverter.toLE(n, from, true);
+              const target = [...BaseConverter.toLE(n, to, false)];
+              
+              const test = [...cb2.convertLE(source)];
+              expect(test).to.eql(target);
+
+              const m = BaseConverter.fromLE(test, to, false);
+              expect(''+m).to.eql(''+n);
+            });
+          }
+        });
+      }
     }
   }
 });
